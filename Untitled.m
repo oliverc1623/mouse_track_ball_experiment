5282c4e7dc58d0db95e2e33c099e07df4099a67b
Oliver <oliverc1622@gmail.com>
2019-07-03 17:06:26 -0700

    Renozs code

--- Untitled.m
+++ Untitled.m
@@ -9,17 +9,24 @@ cam2_timeup = timestamps(cam2_frames);
 laser_frames = intersect(find(data(6,:) <=1.5) +1,find(data(6,:) >1.5));
 
 %% Look at running
-plot(timestamps, avg_disp, '-')
+plot(timestamps, y_FB, '-')
 hold on
 scatter(timestamps(epoch_starts2), 2*ones(7,1), 'kx')
 
-%% Smooth graphs
-d1 = designfilt('lowpassfir','PassbandFrequency',0.05, ...
+%% Remove position noise
+d1 = designfilt('lowpassfir','PassbandFrequency',0.25, ...
          'StopbandFrequency',0.35,'PassbandRipple',0.5, ...
-         'StopbandAttenuation',65,'DesignMethod','kaiserwin');
-y = filtfilt(d1,double(data(2,:)));
+         'StopbandAttenuation',65,'DesignMethod','kaiserwin',...
+         'SampleRate', 100);
+y_LR = filtfilt(d1,double(data(3,:)));
 
-plot(timestamps, y)
+plot(timestamps, y_L)
+
+%% smooth disp noise
+window = 10000;
+coeff = ones(1,window)/window;
+avg_LR = filter(coeff, 1, data(2,:));
+plot(timestamps(1,:), avg_LR(1,:))
 
 %% object pos frames
 object_timeup = set_timestamp(1, data, 1.5)
@@ -39,6 +46,22 @@ new_laser_frames = laser_frames
 
 for i = 1:length(laser_frames)+1
      new_laser_frames(1,i) = laser_frames(i) - laser_frames(i+1)
+end
+
+%% Movie Code
+Z = peaks;
+surf(Z)
+axis tight manual
+ax = gca;
+ax.NextPlot = 'replaceChildren';
+
+loops = 40;
+F(loops) = struct('cdata',[],'colormap',[]);
+for j = 1:loops
+    X = data(2,j)*Z;
+    surf(X,Z)
+    drawnow
+    F(j) = getframe(gcf);
 end
 
 %% 
